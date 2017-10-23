2257c5366d7d99949769346befd7525cd53b951a
081894brayan <brayanestive18@gmail.com>
2017-10-22 23:32:16 -0500

    V4

---
+++ app/src/main/java/com/brayadiaz/merkapues/MisComprasFragment.java
@@ -0,0 +1,57 @@
+package com.brayadiaz.merkapues;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MisComprasFragment extends Fragment {
+
+    private String Lista[] = new String[]{
+            "Arroz             2 Lb               $5.000",
+            "Azucar          2 Kg               $5.000",
+            "Sal                2 Lb                $1.000",
+            "Leche           3                    $7.000"};
+    private ListView list;
+
+    public MisComprasFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mis_compras, container, false);
+
+        list = (ListView) view.findViewById(R.id.view_mis_compras);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_expandable_list_item_1, Lista);
+        list.setAdapter(adapter);
+        list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity().getApplicationContext(), String.valueOf(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        return view;
+    }
+
+
+}
